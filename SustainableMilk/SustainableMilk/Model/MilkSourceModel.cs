cb540ac19869e0e90503aa738c7835edfa9eb09d
saiyathalifathimabee.m@syncfusion.com <Saiyathalif@17>
2024-03-06 14:36:40 +0530

    updated the  model name

---
+++ SustainableMilk/SustainableMilk/Model/MilkSourceModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SustainableMilk
+{
+    public class MilkSourceModel
+    {
+        public string Source { get; set; }
+        public double WaterQuantity  { get; set; }
+        public double CO2Emission  { get; set; }
+
+        public MilkSourceModel(string source, double waterQuantity, double cO2Emission)
+        {
+            Source = source;
+            WaterQuantity = waterQuantity;
+            CO2Emission = cO2Emission;
+        }
+    }
+}
